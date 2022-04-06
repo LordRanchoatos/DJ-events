28187c9a7bf764e72bcb7e62fc444e5d755607cc
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-04-06 08:11:39 +0100

    events API routes

---
+++ pages/api/events/[slug].js
@@ -0,0 +1,11 @@
+const data = require('./data')
+
+export default function handler(req, res) {
+    const evt = data.events.filter(ev => ev.slug === req.query.slug)
+    if (req.method === 'GET') {
+        res.status(200).json(evt)
+    } else {
+        res.setHeader('Allow', ['GET'])
+        res.status(405).json({ message: `Method ${req.method} is not allowed`})
+    }
+}
