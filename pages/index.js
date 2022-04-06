33279eec4c9ec7bd624172e84f52e975e624b56f
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-04-06 09:20:05 +0100

    getServerSideProps to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
-import { useRouter } from 'next/router'
 import Layout from '@/components/Layout'
+import { API_URL } from '../config/index'
 
 
-export default function Home() {
-   
+export default function Home({ events }) {
+   console.log(events)
   return (
     <div >
       <Layout >
-        <h1>Home </h1>
+        <h1>Upcoming Events </h1>
       </Layout> 
-
+ 
       
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+    console.log(events)
+
+    return {
+      props: {events},
+    }
+  
+} 
